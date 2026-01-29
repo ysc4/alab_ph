94056ff2b48825e07abbefdbe20fe32096c676a6
Jessy Cassandra <jessy.mapanao03@gmail.com>
2026-01-29 17:23:02 +0800

    Fixed date picker handleClick

--- client/app/components/date-selector.tsx
+++ client/app/components/date-selector.tsx
@@ -7,22 +7,15 @@ interface DateSelectorProps {
 }
 
 const DateSelector: React.FC<DateSelectorProps> = ({ onSelect, value }) => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleClick = () => {
-    inputRef.current?.showPicker?.();
-    inputRef.current?.focus();
-  };
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onSelect?.(e.target.value);
   };
 
   return (
-    <div
-      onClick={handleClick}
-      className="relative w-50 cursor-pointer"
-    >
+    <div className="relative w-50 cursor-pointer">
       <input
         ref={inputRef}
         type="date"
@@ -40,7 +33,6 @@ const DateSelector: React.FC<DateSelectorProps> = ({ onSelect, value }) => {
           [&::-webkit-calendar-picker-indicator]:opacity-0
         "
       />
-
       <Calendar
         className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-text-primary w-5 h-5"
       />
