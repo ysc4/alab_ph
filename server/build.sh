a36be80728ece195a480c967bffcbef9aa05e14d
Jessy Cassandra <jessy.mapanao03@gmail.com>
2026-01-29 17:20:44 +0800

    Updated build.sh script

--- server/build.sh
+++ server/build.sh
@@ -5,5 +5,11 @@ npm install
 npm run build
 
 # Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+
+# Copy Python scripts to dist if building to dist
+if [ -d "dist" ]; then
+	mkdir -p dist/services
+	cp src/services/forecast.py dist/services/
+fi
